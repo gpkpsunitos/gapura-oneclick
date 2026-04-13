6a037279cb5fbebcb5565905b26e406ea672eb8b
gpkpsunitos <gpkps.unit.os@gmail.com>
2026-04-13 12:42:40 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -64,7 +64,7 @@ const nextConfig = {
                     key: "Content-Security-Policy",
                     value: [
                         "default-src 'self'",
-                        "script-src 'self' https://vercel.live",
+                        "script-src 'self' 'unsafe-inline' https://vercel.live",
                         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                         "font-src 'self' https://fonts.gstatic.com",
                         "img-src 'self' data: blob: https:",
